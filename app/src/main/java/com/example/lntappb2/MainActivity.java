e76dfb6bcf18759b4bee0b7d9b5864865b33f1d8
abdul <abdul.tanveer@gmail.com>
2020-11-10 09:46:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lntappb2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lntappb2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickHandler(View view) {
+        Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
+        startActivity(hIntent);
+    }
+}
