360d38af6b292e3f9d8f16ca6e71cafb4ebfc9dd
abdul <abdul.tanveer@gmail.com>
2020-11-10 11:36:23 +0530

    lifecycle of an activity debug with breakpoints etc

--- app/src/main/java/com/example/lntappb2/MainActivity.java
+++ app/src/main/java/com/example/lntappb2/MainActivity.java
@@ -18,10 +18,42 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG,"onCreate");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onpause");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onresume");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onstop");
+
+    }
+
     public void clickHandler(View view) {
         Log.e(TAG,"clickHandler");
         Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
         hIntent.putExtra("mykey","abdul");
+        int c = add(10,20);
         startActivity(hIntent);
+    }
+
+    private int add(int a, int b) {
+        return a+b;
     }
 }
