f8ea93c9224723525eaf42faaec96f2b01828ef2
abdul <abdul.tanveer@gmail.com>
2020-11-10 10:31:40 +0530

    logcat and logs

--- app/src/main/java/com/example/lntappb2/HomeActivity.java
+++ app/src/main/java/com/example/lntappb2/HomeActivity.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = HomeActivity.class.getSimpleName() ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.w(TAG,"onCreate");
         //get the intent which started this activity
         Intent intent = getIntent();
         //from the intent get the extras
--- app/src/main/java/com/example/lntappb2/MainActivity.java
+++ app/src/main/java/com/example/lntappb2/MainActivity.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName() ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG,"onCreate");
     }
 
     public void clickHandler(View view) {
+        Log.e(TAG,"clickHandler");
         Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
         hIntent.putExtra("mykey","abdul");
         startActivity(hIntent);
