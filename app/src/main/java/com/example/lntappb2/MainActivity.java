e5a91f8215d07bb5f120e658143c310e293b4440
abdul <abdul.tanveer@gmail.com>
2020-11-10 12:04:21 +0530

    implicit intents

--- app/src/main/java/com/example/lntappb2/MainActivity.java
+++ app/src/main/java/com/example/lntappb2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lntappb2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -47,7 +48,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickHandler(View view) {
         Log.e(TAG,"clickHandler");
-        Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
+
+        switch (view.getId()){
+            case R.id.buttonlogin:
+                startHome();
+                break;
+            case R.id.buttoncancel:
+                Intent dialIntent =new Intent(Intent.ACTION_VIEW,  Uri.parse("tel:12345678" ));
+                        //"//http://www.google.com"));
+                        //new Intent(Intent.ACTION_DIAL, Uri.parse("tel:1234567"));
+                startActivity(dialIntent);
+                break;
+        }
+    }
+
+    private void startHome() {
+        Intent hIntent = new Intent(MainActivity.this, HomeActivity.class);
         hIntent.putExtra("mykey","abdul");
         int c = add(10,20);
         startActivity(hIntent);
