8318d1daa31efc5f17ea9b919ea17220befd83e7
abdul <abdul.tanveer@gmail.com>
2020-11-11 12:20:10 +0530

    who wrote it part2

--- app/src/main/java/com/example/lntappb2/AsyncActivity.java
+++ app/src/main/java/com/example/lntappb2/AsyncActivity.java
@@ -7,25 +7,31 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class AsyncActivity extends AppCompatActivity {
     private static final String TAG = AsyncActivity.class.getSimpleName();
     ProgressBar progressBar;
     EditText mBookInput;
+    TextView mTitleText,mAuthorText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async);
         progressBar = findViewById(R.id.progressBar);
         mBookInput = findViewById(R.id.bookInput);
-        String queryString = mBookInput.getText().toString();
+        mTitleText = findViewById(R.id.titleText);
+        mAuthorText = findViewById(R.id.textViewauthor);
     }
 
     public void handleClick(View view) {
         Log.i(TAG,"handleClick");
-        //download something from internet
+      /*  //download something from internet
         DownloadTask downloadTask = new DownloadTask(progressBar);
         downloadTask.execute("https://urlForimagetobedownloaded");
-        //show the download progress on the progressbar
+        //show the download progress on the progressbar*/
+        String queryString = mBookInput.getText().toString();
+
+        new FetchBook(mTitleText, mAuthorText).execute(queryString);
     }
 }
