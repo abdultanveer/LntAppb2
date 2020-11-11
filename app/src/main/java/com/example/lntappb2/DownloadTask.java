a571b5f64661895944e7121270a8ed49027067b5
abdul <abdul.tanveer@gmail.com>
2020-11-11 11:40:46 +0530

    asynctask part2

--- app/src/main/java/com/example/lntappb2/DownloadTask.java
+++ app/src/main/java/com/example/lntappb2/DownloadTask.java
@@ -6,10 +6,11 @@ import android.widget.ProgressBar;
 
 //input type, progress type,result type
 public class DownloadTask extends AsyncTask<String,Integer,Void> {
-
+    ProgressBar mProgressBar;
     private static final String TAG = DownloadTask.class.getSimpleName() ;
 
     public DownloadTask(ProgressBar progressBar) {
+        mProgressBar = progressBar;
     }
 
     /**
@@ -19,7 +20,14 @@ public class DownloadTask extends AsyncTask<String,Integer,Void> {
      */
     @Override
     protected Void doInBackground(String... strings) {
-        Log.i(TAG,strings[0]);
+        Log.i(TAG,"doInBackground"+strings[0]);
+        publishProgress(50);
         return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        mProgressBar.setProgress(values[0]);
     }
 }
