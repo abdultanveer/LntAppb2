ed588f9bf08cc533ab49bfe09ce2a7f7fed32926
abdul <abdul.tanveer@gmail.com>
2020-11-11 16:39:01 +0530

    services part2 -- playing music

--- app/src/main/java/com/example/lntappb2/MusicService.java
+++ app/src/main/java/com/example/lntappb2/MusicService.java
@@ -2,6 +2,7 @@ package com.example.lntappb2;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.widget.Toast;
 
@@ -13,6 +14,14 @@ public class MusicService extends Service {
     public void onCreate() {
         super.onCreate();
         Toast.makeText(this, "service created", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+         super.onStartCommand(intent, flags, startId);
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        mediaPlayer.start();
+        return START_STICKY;
     }
 
     @Override
