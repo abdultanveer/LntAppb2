a5fe3c5de08a1846624397d84dcdada32e9754ad
abdul <abdul.tanveer@gmail.com>
2020-11-11 12:29:49 +0530

    who wrote it part3

--- app/src/main/java/com/example/lntappb2/FetchBook.java
+++ app/src/main/java/com/example/lntappb2/FetchBook.java
@@ -3,6 +3,10 @@ package com.example.lntappb2;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class FetchBook extends AsyncTask<String,Void,String> {
 private TextView mTitleText;
 private TextView mAuthorText;
@@ -15,7 +19,34 @@ protected String doInBackground(String... params) {
     return NetworkUtils.getBookInfo(params[0]);
 }
 @Override
-protected void onPostExecute(String s) {
+protected void onPostExecute(String s)
+{
 super.onPostExecute(s);
+    try {
+        JSONObject jsonObject = new JSONObject(s);
+        JSONArray itemsArray = jsonObject.getJSONArray("items");
+        //Iterate through the results
+        for(int i = 0; i<itemsArray.length(); i++){
+            JSONObject book = itemsArray.getJSONObject(i); //Get the current item
+            String title=null;
+            String authors=null;
+            JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+            try {
+                title = volumeInfo.getString("title");
+                authors = volumeInfo.getString("authors");
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+//If both a title and author exist, update the TextViews and return
+            if (title != null && authors != null){
+                mTitleText.setText(title);
+                mAuthorText.setText(authors);
+                return;
+            }
+        }
+
+    } catch (JSONException e) {
+        e.printStackTrace();
+    }
 }
 }
