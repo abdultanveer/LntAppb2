455bde3534022eb397ac228ba46744cf47a55003
abdul <abdul.tanveer@gmail.com>
2020-11-11 15:01:38 +0530

    sms broadcast receiver with sms body in toast

--- app/src/main/java/com/example/lntappb2/SmsReceiver.java
+++ app/src/main/java/com/example/lntappb2/SmsReceiver.java
@@ -3,6 +3,8 @@ package com.example.lntappb2;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
 import android.widget.Toast;
 
 public class SmsReceiver extends BroadcastReceiver {
@@ -11,6 +13,22 @@ public class SmsReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        Toast.makeText(context,"abdul u've recvd sms",Toast.LENGTH_SHORT).show();
+        if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
+            Bundle bundle = intent.getExtras();
+            SmsMessage[] msgs = null;
+            String msg_from;
+            String msgBody;
+            if (bundle != null) {
+
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    msgs = new SmsMessage[pdus.length];
+                    for (int i = 0; i < msgs.length; i++) {
+                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                        msg_from = msgs[i].getOriginatingAddress();
+                        msgBody = msgs[i].getMessageBody();
+                        Toast.makeText(context, "SenderNum :" + msg_from + "\nmsg :" + msgBody, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        }
     }
-}
