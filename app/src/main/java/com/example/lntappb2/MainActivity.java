683edb32a0429f9400cf7e9fe40b381bec1a5ed7
abdul <abdul.tanveer@gmail.com>
2020-11-12 09:37:35 +0530

    sharedprefs

--- app/src/main/java/com/example/lntappb2/MainActivity.java
+++ app/src/main/java/com/example/lntappb2/MainActivity.java
@@ -3,20 +3,27 @@ package com.example.lntappb2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName() ;
-
+    public static final String MYPREFS = "myprefs";
+    public static final String NAMEKEY = "namekey";
+    public static final String PWDKEY = "pwdkey";
+    EditText nameEditText,pwdEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG,"onCreate");
+        nameEditText =  findViewById(R.id.editTextName);
+        pwdEditText = findViewById(R.id.editTextPwd);
     }
 
     @Override
@@ -29,14 +36,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.i(TAG,"onpause");
+        saveData();
+    }
 
+    /**
+     * this method will save data from edittexts into a sharedprefs
+     */
+    private void saveData() {
+        Log.i(TAG,"saveData");
+
+        //get the data from the edittext
+        String name = nameEditText.getText().toString();
+        String pwd = pwdEditText.getText().toString();
+        //create a file names myprefs
+        SharedPreferences preferences = getSharedPreferences(MYPREFS,MODE_PRIVATE);
+        //open the file
+        SharedPreferences.Editor editor = preferences.edit();
+        //write to the file
+        editor.putString(NAMEKEY,name);
+        editor.putString(PWDKEY,pwd);
+        //save the file
+        editor.apply();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.i(TAG,"onresume");
+        restoreData();
+    }
 
+    private void restoreData(){
+        Log.i(TAG,"restoreData");
+
+        //open the file
+        SharedPreferences preferences = getSharedPreferences(MYPREFS,MODE_PRIVATE);
+        //read the file
+        String name = preferences.getString(NAMEKEY,"");
+        String pwd = preferences.getString(PWDKEY,"");
+        //set the data in edittexts
+        nameEditText.setText(name);
+        pwdEditText.setText(pwd);
     }
 
     @Override
